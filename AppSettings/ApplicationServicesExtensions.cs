969dce9673f29eb54af38c90d0b1925c168c8746
dihlogg <dinhlongg171204@gmail.com>
2025-02-22 19:42:00 +0700

    #update-leave-stt

--- AppSettings/ApplicationServicesExtensions.cs
+++ AppSettings/ApplicationServicesExtensions.cs
@@ -20,6 +20,7 @@ public static class ApplicationServicesExtensions
         services.AddTransient<ILeaveServive, LeaveService>();
         services.AddTransient<ILeaveDashboardCardRepository, LeaveDashboardCardRepository>();
         services.AddTransient<IRequestReasonRepository, RequestReasonRepository>();
+        services.AddTransient<IRequestStatusRepository, RequestStatusRepository>();
         //services.AddTransient<IAuthenticationService, AuthenticationService>();
     }
 }
